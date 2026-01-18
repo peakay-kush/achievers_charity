7641c7ffdb5ba56706c9fba49ea06985a2e9b83a
peakay-kush <kushpeekay@gmail.com>
2026-01-18 20:43:08 +0300

    Update sitemap and robots.ts with custom domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = 'https://achievers-charity.vercel.app'
+    const baseUrl = 'https://achieverscharitygroup.co.ke'
 
     return {
         rules: {
